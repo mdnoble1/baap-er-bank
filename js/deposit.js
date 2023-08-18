2ff9cc2a6430065945385d8d01144b8f6669c9d4
Md Noble <mdnoble.wd@gmail.com>
2023-08-18 12:39:27 +0600

    added deposit amount calculations

---
+++ js/deposit.js
@@ -0,0 +1,33 @@
+document.getElementById('button-deposit').addEventListener('click' , function(event){
+    // deposit input 
+
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    // deposit box 
+
+    const currentDepositField = document.getElementById('deposit-amount');
+    const oldDepositAmountString = currentDepositField.innerText;
+    const oldDepositAmount = parseFloat(oldDepositAmountString);
+
+    // deposit calculation 
+    
+    const currentDepositAmount = oldDepositAmount + newDepositAmount;
+    currentDepositField.innerText = currentDepositAmount;
+
+    // total balance box 
+
+    const balanceField = document.getElementById('balance-amount');
+    const oldBalanceAmountString = balanceField.innerText;
+    const oldBalanceAmount = parseFloat(oldBalanceAmountString);
+
+    // total balance calculation 
+
+    const newBalance = oldBalanceAmount + newDepositAmount;
+    balanceField.innerText = newBalance;
+
+    // clear input field 
+
+    depositField.value = '';
+})
